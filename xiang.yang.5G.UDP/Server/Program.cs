3020bbafe60e014055911271f7222bd3afafc2a4
Yang Xiang <yangxiangpro@gmail.com>
2020-05-07 18:15:04 +0200

    Add files via upload

---
+++ xiang.yang.5G.UDP/Server/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace udpserver
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("server in attesa di connessione");
+            UdpClient client = new UdpClient(8080);
+
+            IPEndPoint ipremoto = new IPEndPoint(IPAddress.Any, 8080);
+
+            
+            while (true)
+            {
+                //Ricevo il messaggio dal client
+                byte[] receivedbytes = client.Receive(ref ipremoto);
+
+                if (receivedbytes != null)
+                {
+                    if (Encoding.ASCII.GetString(receivedbytes) == "stop server")
+                    {
+                        Console.WriteLine("Chiusura del programma!");
+                        break;
+                    }
+                    else
+                    {
+                        //Traduco il messaggio (in codifica ASCII) e lo visualizzo
+                        string messaggio = Encoding.ASCII.GetString(receivedbytes);
+                        Console.WriteLine("messaggio ricevuto: " + messaggio);
+                        //Invio l'eco al client
+                        string inputs = messaggio;
+                        byte[] bytesent = Encoding.ASCII.GetBytes(inputs);
+                        client.Send(bytesent, bytesent.Length, ipremoto);
+                        Console.WriteLine("L'eco è stato inviato");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("messaggio ricevuto vuoto");
+                }
+            }
+            //Chiudo
+            Console.WriteLine("Premi un tasto qualsiasi per chiudere...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ xiang.yang.5G.UDP/xiang.yang.5G.UDP/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPclient
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint ipremoto = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            client.Connect(ipremoto);
+
+            Console.Write(">");
+            string inputs = Console.ReadLine();
+            if (inputs != null)
+            {
+                //Invio il messaggio al server
+                byte[] bytesent = Encoding.ASCII.GetBytes(inputs);
+                client.Send(bytesent, bytesent.Length);
+                Console.WriteLine("il messaggio è stato inviato");
+                //Adesso resto in ascolto dell'eco dal server
+                byte[] receivedbytes = client.Receive(ref ipremoto);
+                string messaggio = Encoding.ASCII.GetString(receivedbytes);
+                Console.WriteLine("eco ricevuto: " + messaggio);
+                //Invio "stop server" al Server
+                bytesent = Encoding.ASCII.GetBytes("stop server");
+                client.Send(bytesent, bytesent.Length);
+                Console.WriteLine("Ho inviato 'stop server'");
+                //Chiudo
+                client.Close();
+                Console.WriteLine("Premi un tasto qualsiasi per chiudere...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
